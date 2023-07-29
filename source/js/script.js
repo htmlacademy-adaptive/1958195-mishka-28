76d9234dca77f2eb5a3304c990180c08bed9028b
Alimov Farruh <linsheng1990@gmail.com>
2023-07-29 15:51:59 +0300

    Стилизует главную страницу

--- source/js/script.js
+++ source/js/script.js
@@ -1,12 +1,16 @@
 let headerMenu = document.querySelector('.main-menu');
 let menuButton = document.querySelector('.main-menu__button');
-// let header = document.querySelector('.header__wrapper');
-// let addressMap = document.querySelector('.address__map');
+let header = document.querySelector('.header__top-wrapper');
+let contactMap = document.querySelector('.contacts__map');
 
 headerMenu.classList.remove('main-menu--nojs');
-// header.classList.remove('header__wrapper--nojs');
-// addressMap.classList.remove('address__map--nojs');
+header.classList.remove('header__top-wrapper--nojs');
+contactMap.classList.remove('contacts__map--nojs');
 
 menuButton.addEventListener('click', function () {
   headerMenu.classList.toggle('main-menu--active')
+});
+
+menuButton.addEventListener('click', function () {
+  menuButton.classList.toggle('main-menu__button--active')
 });
