6f4fe36a235ae6a18cb213447d00d910c3c6b5ff
Alimov Farruh <linsheng1990@gmail.com>
2023-08-03 19:33:23 +0300

    Завершает верстку всех страниц

--- source/js/script.js
+++ source/js/script.js
@@ -1,16 +1,17 @@
 let headerMenu = document.querySelector('.main-menu');
-let menuButton = document.querySelector('.main-menu__button');
+let menuButton = document.querySelector('.burger-button');
 let header = document.querySelector('.header__top-wrapper');
 let contactMap = document.querySelector('.contacts__map');
 
+menuButton.classList.remove('burger-button--nojs');
 headerMenu.classList.remove('main-menu--nojs');
 header.classList.remove('header__top-wrapper--nojs');
-contactMap.classList.remove('contacts__map--nojs');
+contactMap?.classList.remove('contacts__map--nojs');
 
 menuButton.addEventListener('click', function () {
   headerMenu.classList.toggle('main-menu--active')
 });
 
 menuButton.addEventListener('click', function () {
-  menuButton.classList.toggle('main-menu__button--active')
+  menuButton.classList.toggle('burger-button--active')
 });
